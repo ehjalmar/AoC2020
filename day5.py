00ff56dc83a804b9c33beaedc09d135651d70770
Erik Hjalmarsson <edde_888@hotmail.com>
2020-12-05 16:32:32 +0100

    - Added day 5

---
+++ day5.py
@@ -0,0 +1,48 @@
+file=open('day5input.txt', 'r')
+lines = file.readlines()
+currentIndex = 0
+boardingPasses = []
+
+for line in lines:
+    
+    charPosition = 0
+    rowNumber = 0
+    maxRow = 127
+    minRow = 0
+    maxSeat = 7
+    minSeat = 0
+    seatNumber = 0
+    # Get row number
+    while charPosition < 7:
+        currentLetter = line[charPosition]
+        currentInterval = maxRow - minRow  + 1
+        if currentLetter == "F":
+            maxRow = (maxRow) - (currentInterval / 2)
+        else:
+            minRow = (minRow) + (currentInterval / 2)
+        charPosition += 1
+    
+    rowNumber = minRow
+
+    # Get seat number
+    while charPosition < 10:
+        currentLetter = line[charPosition]
+        currentInterval = maxSeat - minSeat + 1
+        if currentLetter == "L":
+            maxSeat = (maxSeat) - (currentInterval / 2)
+        else:
+            minSeat = (minSeat) + (currentInterval / 2)
+        charPosition += 1
+
+    seatNumber  = maxSeat
+    
+    # Get seat ID
+    seatId = rowNumber * 8 + seatNumber
+    boardingPasses.append(seatId)
+    print("seatId: " + str(seatId))
+
+
+# order boarding passes
+boardingPasses.sort()
+print("Highest seatId: " + str(boardingPasses[-1]))
+    
